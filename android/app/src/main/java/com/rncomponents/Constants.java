f2f4de173619edbed1dfca82e937400120215d70
汪星宇 <xingyu.wang@ufan.com.cn>
2016-07-12 19:27:40 +0800

    update

--- android/app/src/main/java/com/rncomponents/Constants.java
+++ android/app/src/main/java/com/rncomponents/Constants.java
@@ -9,4 +9,6 @@ public class Constants {
     public static int REQUEST_FOR_PHOTO_PICKER = 201;
     public final static int REQUEST_CODE_PHOTO_CROPPING = 202;
     public static final int REQUEST_COLUMN_PICKER = 203;
+
+    public static int DEFAULT_SET_ADDRESS_OUT_TIME = 1*60*60*1000;//1h
 }
